5b2ee15c8b669c8741b0dfde6ba6c52f2c5eff96
Kurniawan <kurniawanronaldi@gmail.com>
2023-01-18 17:11:23 +0700

    improving client response

--- api/app/Http/Controllers/ClientController.php
+++ api/app/Http/Controllers/ClientController.php
@@ -10,7 +10,12 @@ class ClientController extends Controller
 {
     public function find_all()
     {
-        return new ClientResource(Client::all());
+        $data = Client::all();
+        foreach($data as $k => $d){
+            $r = Client::findOrFail($d->id);
+            $data[$k]->module = $r->module()->first();
+        }
+        return new ClientResource($data);
     }
 
     public function create(Request $request)
@@ -24,12 +29,17 @@ class ClientController extends Controller
             'module_id' => $request->module_id,
             'result' => $request->result
         ]);
+        if(isset($request->module_id)){
+            $r = Client::findOrFail($data->id);
+            $data->module = $r->module()->first();
+        }
         return new ClientResource($data);
     }
 
     public function find_by_id($id)
     {
         $data = Client::findOrFail($id);
+        $data->module = $data->module()->first();
         return new ClientResource($data);
     }
 
